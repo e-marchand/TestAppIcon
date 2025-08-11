b9dd063dbe8df872ab037f03bbffcb32b96f11f1
eric.marchand <eric.marchand@4d.com>
2025-08-11 17:24:29 +0200

    Initial Commit

---
+++ TestAppIcon/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestAppIcon
+//
+//  Created by emarchand on 11/08/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
